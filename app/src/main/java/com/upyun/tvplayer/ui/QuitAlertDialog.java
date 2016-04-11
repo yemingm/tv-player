df52f4a29f009fba51cf1668809ed813e556a653
mingming.ye <mingming.ye@upai.com>
2016-04-11 17:14:13 +0800

    update UI

--- app/src/main/java/com/upyun/tvplayer/ui/QuitAlertDialog.java
+++ app/src/main/java/com/upyun/tvplayer/ui/QuitAlertDialog.java
@@ -4,8 +4,10 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Process;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.upyun.tvplayer.R;
 import com.upyun.tvplayer.util.LocalSave;
@@ -29,8 +31,8 @@ public class QuitAlertDialog extends Dialog {
             @Override
             public void onClick(View v) {
                 QuitAlertDialog.this.dismiss();
-                LocalSave.saveChannel(getContext(),MyApplication.channel);
-                LocalSave.saveProgram(getContext(),MyApplication.programList);
+                LocalSave.saveChannel(getContext(), MyApplication.channel);
+                LocalSave.saveProgram(getContext(), MyApplication.programList);
                 android.os.Process.killProcess(Process.myPid());
             }
         });
@@ -40,6 +42,14 @@ public class QuitAlertDialog extends Dialog {
                 QuitAlertDialog.this.dismiss();
             }
         });
+
+        Window dialogWindow = getWindow();
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        dialogWindow.setGravity(Gravity.CENTER);
+        lp.width = getContext().getResources().getDimensionPixelSize(R.dimen.dialog_width);
+        lp.height = getContext().getResources().getDimensionPixelSize(R.dimen.dialog_height);
+        lp.alpha = 0.6f;
+        dialogWindow.setAttributes(lp);
     }
 
     public static void show(Context context) {
